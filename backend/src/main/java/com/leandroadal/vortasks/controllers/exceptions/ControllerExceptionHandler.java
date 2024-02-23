2d1dbe5e31f0e05eaca287b618b1bd6ffcc44ffc
Leandro Silva <lleandro.adal@gmail.com>
2024-02-23 00:47:48 -0300

    Fixed exception handling for custom validation annotations.

--- backend/src/main/java/com/leandroadal/vortasks/controllers/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/leandroadal/vortasks/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.leandroadal.vortasks.controllers.exceptions;
 
 import java.time.Instant;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -61,6 +65,17 @@ public class ControllerExceptionHandler {
         return ResponseEntity.status(status).body(err);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        String error = "Erro ao validar";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+			err.addError(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+        return ResponseEntity.status(status).body(err);
+    }
+
     // ----------------------- BACKUP -----------------------
 
     @ExceptionHandler(BackupCreationException.class)
@@ -93,6 +108,14 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
+        String error = "Erro no banco de dados";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> integrityViolation(DataIntegrityViolationException e, HttpServletRequest request) {
         String error = "Erro no banco de dados";
         HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
